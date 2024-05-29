46ee24ae1633b6abef49c370d9f472507098a1f4
hemant1317 <hemant1317@gmail.com>
2024-05-30 00:39:50 +0530

    day3 assignment commit

--- src/main/java/com/example/StudentGradeEvaluation.java
+++ src/main/java/com/example/StudentGradeEvaluation.java
@@ -4,30 +4,44 @@ import java.util.Scanner;
 
 public class StudentGradeEvaluation {
     public static void main(String[] args) {
+
+        // Create a scanner object to read input from the console
         Scanner scanner = new Scanner(System.in);
 
         // Prompt the user to enter their name
         System.out.println("Enter your name: ");
-        
-        // Have the program read the user name and deliver output
+        // Read the input provided by the user for name
+        String name = scanner.nextLine();
+        // Print the user's name
+        System.out.println("Your name " + name);
 
-String name = scanner.nextLine();
-System.out.println("Your name " + name);
         // Prompt the user to enter their age
         System.out.println("Enter your age: ");
-
-        // Have the program read the user age and deliver output
+        // Read the integer input provided by the user for age
         int age = scanner.nextInt();
+        // Print the user's age
         System.out.println("Your age " + age);
+
         // Prompt the user to enter their exam score
-        System.out.println("Enter your score: ");
-
-        //Have the program read user score and deliver output
-Double score = scanner.nextDouble();
-        // Determine the grade
-        String grade;
+        System.out.println("Enter your exam score: ");
+        // Read the double input provided by the user for score
+        Double score = scanner.nextDouble();
+        // Print the user's score
+        System.out.println("Your exam score " + score);
         
-
+        // Determine the grade
+        if (score >= 90) {
+            System.out.println("Grade: A");
+        } else if (score >= 80 && score <=89) {
+            System.out.println("Grade: B");
+            } else if (score >= 70 && score <= 79) {
+                System.out.println("Grade: C");
+            }  else if (score >= 60 && score <= 69) {
+                    System.out.println("Grade: D");
+            }    else {
+                        System.out.println("Grade: F");
+            }
+        
         // Print the student's details
         
 
