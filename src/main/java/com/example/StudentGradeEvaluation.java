3fca7b47cd4d2bb424037e5b5c2a38a18e9c50b2
hemant1317 <hemant1317@gmail.com>
2024-05-29 08:48:48 +0530

    assignment day 3

--- src/main/java/com/example/StudentGradeEvaluation.java
+++ src/main/java/com/example/StudentGradeEvaluation.java
@@ -7,15 +7,23 @@ public class StudentGradeEvaluation {
         Scanner scanner = new Scanner(System.in);
 
         // Prompt the user to enter their name
-        System.out.print("Enter your name: ");
+        System.out.println("Enter your name: ");
         
+        // Have the program read the user name and deliver output
 
+String name = scanner.nextLine();
+System.out.println("Your name " + name);
         // Prompt the user to enter their age
-        
+        System.out.println("Enter your age: ");
 
+        // Have the program read the user age and deliver output
+        int age = scanner.nextInt();
+        System.out.println("Your age " + age);
         // Prompt the user to enter their exam score
-        
+        System.out.println("Enter your score: ");
 
+        //Have the program read user score and deliver output
+Double score = scanner.nextDouble();
         // Determine the grade
         String grade;
         
